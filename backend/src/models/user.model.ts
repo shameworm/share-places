7a0eab83bce63a50edfd5cf615d48b895f394d71
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2024-10-05 16:01:14 +0300

    feat(backend/models): add user mongoose schema

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,6 +1,23 @@
-export interface User {
+import mongoose, { Document, Schema } from "mongoose";
+import mongooseUniqueValidator from "mongoose-unique-validator";
+interface IUser extends Document {
   id: string;
   name: string;
+  email: string;
+  password: string;
   image: string;
-  places: number;
+  places: string;
 }
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 5 },
+  image: { type: String, required: true },
+  places: { type: String, require: true },
+});
+
+userSchema.plugin(mongooseUniqueValidator);
+
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
