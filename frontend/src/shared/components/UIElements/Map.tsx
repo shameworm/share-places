0f54929b3502b717606b004ac57bc8df1f5f034b
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2024-12-27 16:12:50 +0200

    refactor(frontend/map): replace GoogleMap with OpenLayer map

--- frontend/src/shared/components/UIElements/Map.tsx
+++ frontend/src/shared/components/UIElements/Map.tsx
@@ -1,32 +1,79 @@
-import { useEffect, useRef } from 'react';
+import { useRef, useEffect, forwardRef, useImperativeHandle } from "react";
+
+import "ol/ol.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { Icon, Style } from "ol/style";
+import { useGeographic } from "ol/proj";
+
+import MarkerImg from "../../../assets/marker.svg";
 
 interface PlaceMapProps {
   center: { lat: number; lng: number };
   zoom: number;
 }
 
-const Map: React.FC<PlaceMapProps> = ({ center, zoom }) => {
-  const mapRef = useRef<HTMLDivElement | null>(null);
+const PlaceMap = forwardRef<HTMLDivElement, PlaceMapProps>(
+  ({ center, zoom }, ref) => {
+    const mapRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    function initMap(): void {
-      const Map = google.maps.Map;
-      const map = new Map(mapRef.current as HTMLDivElement, {
-        center,
-        zoom,
+    useGeographic();
+
+    useEffect(() => {
+      if (!mapRef.current) return;
+
+      const map = new Map({
+        target: mapRef.current,
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+        ],
+        view: new View({
+          center: [center.lng, center.lat],
+          zoom,
+        }),
       });
-      new google.maps.Marker({
-        position: center,
-        map,
+
+      const marker = new Feature({
+        geometry: new Point([center.lng, center.lat]),
       });
-    }
 
-    google.maps.importLibrary('maps').then(() => {
-      initMap();
-    });
-  }, [center, zoom]);
+      const markerStyle = new Style({
+        image: new Icon({
+          src: MarkerImg,
+          anchor: [0.5, 1],
+          anchorXUnits: "fraction",
+          anchorYUnits: "fraction",
+          scale: 1,
+        }),
+      });
 
-  return <div ref={mapRef} id="map"></div>;
-};
+      marker.setStyle(markerStyle);
 
-export default Map;
+      const markerLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [marker],
+        }),
+      });
+
+      map.addLayer(markerLayer);
+
+      return () => {
+        map.setTarget(undefined);
+      };
+    }, [center, zoom]);
+
+    useImperativeHandle(ref, () => mapRef.current!);
+
+    return <div ref={mapRef} id="map" />;
+  },
+);
+
+export default PlaceMap;
