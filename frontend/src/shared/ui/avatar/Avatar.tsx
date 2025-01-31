cffd42ba8811c85a01b1f09de5e6698a5c4fffd0
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2025-01-31 13:15:40 +0200

    feat(frontend/shared/avatar): add avatar stories for storybook

--- frontend/src/shared/ui/avatar/Avatar.tsx
+++ frontend/src/shared/ui/avatar/Avatar.tsx
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-accent-foreground text-background",
+      "flex h-full w-full items-center justify-center rounded-full bg-accent-foreground text-accent",
       className,
     )}
     {...props}
