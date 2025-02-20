1e666ff9c97f90676ebd28880b0f53691cfa820f
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2025-02-20 12:52:41 +0200

    refactor(entities/place): remove unused fields from PlaceCard stories

--- frontend/src/entities/place/ui/PlaceCard.stories.tsx
+++ frontend/src/entities/place/ui/PlaceCard.stories.tsx
@@ -10,11 +10,7 @@ const meta = {
     title: { control: "text" },
     description: { control: "text" },
     address: { control: "text" },
-    creatorId: { control: "text" },
-    coordinates: {
-      control: "object",
-      defaultValue: { lat: 0, lng: 0 },
-    },
+    creator: { control: "text" },
   },
   parameters: {
     layout: "centered",
@@ -33,10 +29,6 @@ export const Basic: Story = {
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
     address: "20 W 34th St, New York, NY 10001",
-    coordinates: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creatorId: "u1",
+    creator: "u1",
   },
 };
