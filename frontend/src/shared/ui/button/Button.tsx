a205ab4e91910b3d70a93866ccfde43c73d8797e
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2025-02-21 18:52:03 +0200

    feat(shared/button): change text size on button

--- frontend/src/shared/ui/button/Button.tsx
+++ frontend/src/shared/ui/button/Button.tsx
@@ -21,9 +21,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        base: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        base: "h-9 px-4 py-2 text-lg font-semibold",
+        sm: "h-8 rounded-md px-3 text-xs text-lg font-semibold",
+        lg: "h-10 rounded-md px-8 text-lg font-semibold",
         icon: "h-9 w-9",
       },
     },
